95865565ef8924481398d2e2206db4ee7ddc5ee8
Gabor Abraham <godgab@gmail.com>
2022-05-14 22:43:29 +0200

    Implement sortedView

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,41 @@
+#include <algorithm>
+#include <array>
 #include <iostream>
+#include <sstream>
+
+#include "sortedView.hpp"
+
+/**
+ * @brief Represents a large object
+ */
+struct LargeObject {
+	LargeObject() = default;
+
+	LargeObject(uint64_t num)
+		: order{num}
+		{}
+
+	// Copy operation is expensive, disable it
+	LargeObject(const LargeObject&) = delete;
+	LargeObject& operator=(const LargeObject&) = delete;
+
+	// Move could be fine, but example should not use it!
+	LargeObject(LargeObject&&) = delete;
+	LargeObject& operator=(LargeObject&&) = delete;
+
+	uint64_t getOrder() const {
+		return order;
+	}
+
+	// TODO: Use three-way comparison when using C++20
+	bool operator<(const LargeObject& rhs) const {
+		return this->getOrder() < rhs.getOrder();
+	}
+
+private:
+	uint64_t order = 0;
+};
+
 
 /**
  * @brief Example program entry point
@@ -7,6 +44,46 @@
  */
 int main() {
 	std::cout << "Hello" << std::endl;
+
+	// Allocating large objects in the stack is always a bad idea,
+	// but I would like to try the array deduction
+	std::array collection{
+		LargeObject{47},
+		LargeObject{91},
+		LargeObject{20},
+		LargeObject{40},
+		LargeObject{8}
+	};
+
+	std::cout << "Default order of elements" << std::endl;
+
+	{	// Capture this block so it is not interfering with others
+		std::ostringstream listElements;
+
+		for (const auto& el : collection) {
+			listElements << el.getOrder() << ", ";
+		}
+
+		std::cout << "[" << listElements.str() << "]" << std::endl;
+	}
+
+	{
+		// TODO: Avoid explicit type declaration with type deduction
+		const auto sortedCollection = sortedView<decltype(collection)>(std::cbegin(collection), std::cend(collection));
+
+		/* TODO: Use boost to avoid trailing comma (if I remember correctly, it is something like)
+		boost::algorithm::join(collection | boost::adaptors::transformed(
+			[](const auto& largeObject) { return largeObject.get().getOrder(); }
+		), ", ");
+		*/
+
+		std::ostringstream listElements;
+		for (const auto& el : sortedCollection) {
+			listElements << el.get().getOrder() << ", ";
+		}
+
+		std::cout << "[" << listElements.str() << "]" << std::endl;
+	}
 
 	return 0;
 }
---
+++ src/sortedView.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <cstdlib>
+#include <functional>
+#include <iterator>
+#include <vector>
+
+
+// TODO: SFINAE with operator> and operator< checking
+// Use C++20 concepts?
+/**
+ * @brief Create a vector of reference_wrappers in an incremental order from the iterator range 
+ * 
+ * @tparam Container container type to order
+ * @tparam Container::const_iterator container from and to iterators 
+ * @param from iterator start
+ * @param to iterator end
+ * @return auto vector of reference_wrappers to the object
+ */
+template<typename Container, typename ConstIt = typename Container::const_iterator>
+auto sortedView(ConstIt from, ConstIt to) {
+	std::vector<std::reference_wrapper<const typename Container::value_type> > viewContainer;
+
+	// Allocate space for each element
+	const auto itemCount = static_cast<std::size_t>(std::abs(std::distance(from, to)));
+	viewContainer.reserve(itemCount);
+
+	// Insert a const ref to each element
+	std::transform(from, to, std::back_inserter(viewContainer), 
+		[](const auto& el){ return std::cref(el); }
+	);
+
+	// Sort the vector of references in order
+	std::sort(std::begin(viewContainer), std::end(viewContainer), 
+		[](const auto& lhs, const auto& rhs){ return lhs.get() < rhs.get(); }
+	);
+
+	return viewContainer;
+}
